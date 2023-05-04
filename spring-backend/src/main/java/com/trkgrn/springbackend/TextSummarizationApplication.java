b1b46ae8c6f272e92838e00caa60ae314bd3c34a
trkgrn <takoyaso58@gmail.com>
2023-05-04 15:25:38 +0300

    project started

---
+++ spring-backend/src/main/java/com/trkgrn/springbackend/TextSummarizationApplication.java
@@ -0,0 +1,13 @@
+package com.trkgrn.springbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSummarizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSummarizationApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/trkgrn/springbackend/TextSummarizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trkgrn.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextSummarizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
